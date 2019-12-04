99e6fe7908abbcac3a1bf90ffa0e1caf8f02478d
gnezdilov.ra <Gnezdilov.RA@gutagroup.ru>
2019-12-04 18:46:52 +0300

    feat: implemented getDigitalRoot function

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -281,8 +281,12 @@ function isCreditCardNumber(/* ccn */) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  const n = num.toString().split('').reduce((res, item) => res + Number(item), 0);
+  if (n.toString().length > 1) {
+    return getDigitalRoot(n);
+  }
+  return n;
 }
 
 
