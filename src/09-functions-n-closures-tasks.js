bda726826d0e403ff162b32fb16b04ca60d7edd0
gnezdilov.ra <Gnezdilov.RA@gutagroup.ru>
2019-12-06 13:51:48 +0300

    feat: implemented memoize function

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -83,8 +83,14 @@ function getPolynom() {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let store = null;
+  return function memo() {
+    if (!store) {
+      store = func();
+    }
+    return store;
+  };
 }
 
 
