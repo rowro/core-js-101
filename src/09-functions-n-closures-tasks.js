b2bc5a22d3c8bd92ead336b70e9996f8b74c3a53
gnezdilov.ra <Gnezdilov.RA@gutagroup.ru>
2019-12-06 13:47:40 +0300

    feat: implemented getIdGeneratorFunction function

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -171,8 +171,12 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let count = startFrom - 1;
+  return function idGenerator() {
+    count += 1;
+    return count;
+  };
 }
 
 
