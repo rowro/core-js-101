b8742a7db31d396b989f355cffd67cd202727ac4
gnezdilov.ra <Gnezdilov.RA@gutagroup.ru>
2019-12-16 12:05:19 +0300

    feat: implemented timeSpanToString function

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -74,10 +74,15 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
-}
+function timeSpanToString(startDate, endDate) {
+  const date = new Date(endDate.getTime() - startDate.getTime());
+  const hours = date.getUTCHours().toString().padStart(2, '0');
+  const minutes = date.getUTCMinutes().toString().padStart(2, '0');
+  const seconds = date.getUTCSeconds().toString().padStart(2, '0');
+  const ms = date.getUTCMilliseconds().toString().padStart(3, '0');
 
+  return `${hours}:${minutes}:${seconds}.${ms}`;
+}
 
 /**
  * Returns the angle (in radians) between the hands of an analog clock
