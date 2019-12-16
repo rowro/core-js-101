89e169ba433c4a379fda874ed640c9476bdbdbca
rowro <rowro.dev@gmail.com>
2019-12-16 12:56:07 +0300

    feat: implemented isPrime function

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -189,7 +189,7 @@ function roundToPowerOfTen(/* num, pow */) {
  * See: https://en.wikipedia.org/wiki/Primality_test
  *
  * @param {number} n
- * @return {bool}
+ * @return {boolean}
  *
  * @example:
  *   4 => false
@@ -201,8 +201,15 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n <= 3) return n > 1;
+  if (n % 2 === 0 || n % 3 === 0) return false;
+
+  for (let i = 5; i * i <= n; i += 6) {
+    if (n % i === 0 || n % (i + 2) === 0) return false;
+  }
+
+  return true;
 }
 
 /**
