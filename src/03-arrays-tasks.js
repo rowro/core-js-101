5702a204f3e1dcb734eb27c97977b1cac810d67f
rowro <rowro.dev@gmail.com>
2019-12-20 16:57:14 +0300

    feat: implemented getMovingSum function

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -235,8 +235,15 @@ function toArrayOfSquares(arr) {
  *   [ 0, 0, 0, 0, 0]         => [ 0, 0, 0, 0, 0]
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
-function getMovingSum(/* arr */) {
-  throw new Error('Not implemented');
+function getMovingSum(arr) {
+  const result = [];
+
+  arr.reduce((prevEl, item) => {
+    result.push(prevEl + item);
+    return prevEl + item;
+  }, 0);
+
+  return result;
 }
 
 /**
